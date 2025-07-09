afddd9d3bd65da6d6cc6825ee5499e76331733d1
Abdul Rafay <abdulrafay.chatriwala@gmail.com>
2025-07-09 12:20:42 +0500

    Emabling vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './styles/global.css'; // Your main global styles
+import { Analytics } from "@vercel/analytics/next"
 import 'bootstrap/dist/css/bootstrap.min.css'; // Add Bootstrap CSS
 
 // If Vite generated an index.css or App.css, you might want to keep or merge its content
@@ -10,5 +11,6 @@ import 'bootstrap/dist/css/bootstrap.min.css'; // Add Bootstrap CSS
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <Analytics/>
   </React.StrictMode>,
 );
