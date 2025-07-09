5149d1a4ec1d697eb2a1d5a014b58cca787ec079
Abdul Rafay <abdulrafay.chatriwala@gmail.com>
2025-07-09 12:21:47 +0500

    Enabling React Vercel Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './styles/global.css'; // Your main global styles
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
 import 'bootstrap/dist/css/bootstrap.min.css'; // Add Bootstrap CSS
 
 // If Vite generated an index.css or App.css, you might want to keep or merge its content
